6cfeeef9d918c41654c1d3863ae969e1735c545a
Patrick T <enteepee2@gmail.com>
2022-12-07 18:06:55 +0800

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import Nav from './components/Nav'
-import Hero from './components/Hero'
+import Nav from './components/Nav';
+import Hero from './components/Hero';
 import Card from './components/Card';
-import './App.css'
+// import {v4 as uuid} from "uuid";
+
+import cardData from "../src/data.js";
+
+import './App.css/';
 
 function App() {
+  const cards = cardData.map( data => 
+    <Card 
+      // key={uuid()}
+      key={data.id}
+      data={data}
+    />
+  )
+
   return (
     <div className="App">
       <Nav />
       <Hero />
-      <Card />
+      <section className="cards-list">
+                {cards}
+          </section>
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,30 @@
-const Card = () => (
+const Card = (props) => {
+    const { title, price, coverImg, stats, location, openSpots} = props.data
+    const { rating, reviewCount} = stats
+
+    let badgeText
+    if (openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (location === "Online") {
+        badgeText = "ONLINE"
+    }
+
+    return (
     <>
         <div className="card">
-            <img className="card-image" src="./src/assets/images/katie-zaferes.png" />
+            { badgeText && <div className="card-status">{badgeText}</div>}
+            <img className="card-image" src={`./src/assets/images/${coverImg}`} />
             <div className="card-desc">
                 <div className="card-desc-rating">
                     <img className="card-icon" src="./src/assets/images/star.png" />
-                    <p> 5.0 <span className="text-faded">(6) • USA</span></p>
+                    <p> {rating} <span className="text-faded">({reviewCount}) • {location}</span></p>
                 </div>
-                <p>Life lessons with Katie Zaferes</p>
-                <p><span className="text-bold">From $136 /</span> person</p>    
+                <p className="card-title">{title}</p>
+                <p className="card-price"><span className="text-bold">From ${price} /</span> person</p>    
             </div>    
         </div>
     </>
-)
+)}
 
 
 export default Card;
